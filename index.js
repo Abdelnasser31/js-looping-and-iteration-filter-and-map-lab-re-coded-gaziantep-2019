aec40af820ec26fc7f35f030655aaaef87d89221
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-07-12 08:28:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function driversWithRevenueOver(drivers,revenue){
 }
 
 function dirverNamesWithRevenueOver (drivers, revenue){
-  let copyDrivers = driversWithRevenueOver(drivers,revenue);
-  return copyDrivers.map(driver => driver.name);
+  // let copyDrivers = driversWithRevenueOver(drivers,revenue);
+  // return copyDrivers.map(driver => driver.name);
 }
